2cc29638a954bba071b4cccf7629ee3f002ef65c
jkwon <kwonejk@gmail.com>
2021-11-16 00:55:08 +0800

    1.1 Our First Resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot()
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }), 
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
